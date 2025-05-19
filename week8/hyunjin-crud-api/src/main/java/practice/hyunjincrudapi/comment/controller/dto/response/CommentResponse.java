6a419c0b4def5dd9d524643d56b60aa492ca0852
xxxjinn <tlsguswls319@naver.com>
2025-05-19 14:26:52 +0900

    refactor: @AllArgsConstructor 사용

--- week8/hyunjin-crud-api/src/main/java/practice/hyunjincrudapi/comment/controller/dto/response/CommentResponse.java
+++ week8/hyunjin-crud-api/src/main/java/practice/hyunjincrudapi/comment/controller/dto/response/CommentResponse.java
@@ -1,21 +1,16 @@
 package practice.hyunjincrudapi.comment.controller.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import practice.hyunjincrudapi.comment.entity.Comment;
 
 @Getter
+@AllArgsConstructor
 public class CommentResponse {
     private Long id;
     private String content;
     private Long memberId;
     private Long postId;
-
-    public CommentResponse(Long id, String content, Long memberId, Long postId) {
-        this.id = id;
-        this.content = content;
-        this.memberId = memberId;
-        this.postId = postId;
-    }
 
     public static CommentResponse from(Comment comment) {
         return new CommentResponse(comment.getId(), comment.getContent(), comment.getMember().getId(), comment.getPost().getId());
--- week8/hyunjin-crud-api/src/main/java/practice/hyunjincrudapi/post/controller/dto/response/PostResponse.java
+++ week8/hyunjin-crud-api/src/main/java/practice/hyunjincrudapi/post/controller/dto/response/PostResponse.java
@@ -1,21 +1,16 @@
 package practice.hyunjincrudapi.post.controller.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import practice.hyunjincrudapi.post.entity.Post;
 
 @Getter
+@AllArgsConstructor
 public class PostResponse {
     private Long id;
     private String title;
     private String content;
     private Long memberId;
-
-    public PostResponse(Long id, String title, String content, Long memberId) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.memberId = memberId;
-    }
 
     public static PostResponse from(Post post) {
         return new PostResponse(post.getId(), post.getTitle(), post.getContent(), post.getMember().getId());
